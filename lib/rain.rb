aa78dc9d5a7e95f8e6abef5191319a48d345eade
Vaughn Draughon <vaughn@rocksolidwebdesign.com>
2012-04-08 17:06:27 -0600

    Implemented Equal-Length binning

--- lib/rain.rb
+++ lib/rain.rb
@@ -1,77 +1,253 @@
 require "rain/version"
 
 module Rain
-  # Your code goes here...
   class Discretizer
-
-    def initialize(instances, features, type="object")
-      @discretized_instances = []
-      @cut_points = {}
+    # Initialize {{{
+    def initialize(instances, feature_names, type="equal-length")
       @type = type
+
+      # the discretized  instances, i.e. the  instances that
+      # have had their  continuous-valued features converted
+      # to the appropriate bin numbers
+      @results = []
+
+      # bin boundaries for each feature
+      @feature_cut_points = {}
+
+      # each row of the data set
       @instances = instances
-      @features = features
 
-      @max_arity = @instances.length
-      @max_cut_points = @max_arity - 1
+      # the list of  column names to discretize
+      @feature_names = feature_names
 
-      @columns = {}
+      # the column ov values
+      @feature_values = {}
 
-      # get each feature's list of continuous-valued
-      # attributes as an array
-      @features.each do |f|
-        @columns[f] = @instances.map{ |i| i.send(f) }
-      end
-    end
-
-    def cut_points(feature=nil)
-      if feature.nil?
-        @cut_points
+      # Get each feature's list of continuous-valued
+      # attributes as an array. Accept both an array
+      # of Hashes or an array of Objects such that
+      # features may be provided as methods on an object
+      # such as an ActiveRecord object
+      if @instances.first.is_a?(Hash)
+        @feature_names.each do |f|
+          @feature_values[f] = @instances.map{ |i| i[f] }
+        end
       else
-        @cut_points[feature]
+        @feature_names.each do |f|
+          @feature_values[f] = @instances.map{ |i| i.send(f) }
+        end
+      end
+    end
+    # }}}
+
+    # Utilities {{{
+    # Getters & Setters {{{
+    def features
+      @feature_values
+    end
+
+    def feature_names
+      @feature_names
+    end
+
+    def feature(feature_name=nil)
+      unless feature_name.nil?
+        return @feature_values[feature_name]
+      else
+        []
       end
     end
 
-    def make_cutpoints_equalwidth(arity)
+    def feature_cut_points(feature_name=nil)
+      if feature_name.nil?
+        @feature_cut_points
+      else
+        @feature_cut_points[feature_name]
+      end
+    end
+    # }}}
+
+    # Fit Cutpoints to Population {{{
+    def fit_feature_bins_to_population(cutpoints, values)
+      new_cut_points = []
+
+      # find cut points using the max of each bin
+      cutpoints.each do |p|
+
+        actual_cut_point = values.select{ |a| a <= p }.max()
+
+        # the actual cut point will be the maximum
+        # value found in the real data set that is
+        # less than or equal to the proposed point
+        actual_cut_point = values.select do |a|
+          a <= p
+        end.max()
+
+        new_cut_points << actual_cut_point
+      end
+
+      new_cut_points
+    end
+
+    def fit_feature_bins_to_population!(feature_name)
+      values = feature(feature_name)
+      cutpoints = feature_cut_points(feature_name)
+
+      results = fit_feature_bins_to_population(cutpoints, values)
+
+      @feature_cut_points[feature_name] = results
+    end
+
+    def fit_bins_to_population
+      results = {}
+      @feature_names.each do |f|
+        # get the list of known values for this feature
+        values = feature(f)
+
+        # get the computed cut points for this feature
+        cutpoints = feature_cut_points(f)
+
+        # fit the cutpoints of this feature to the actual
+        # data in the data-set's population
+        results[f] = fit_feature_bins_to_population(cutpoints, values)
+      end
+
+      results
+    end
+
+    def fit_bins_to_population!
+      @feature_cut_points = fit_bins_to_population
+    end
+    # }}}
+    # }}}
+
+    # Discretization Algorithms {{{
+    # Equal Width {{{
+    def make_feature_cutpoints_equalwidth(attributes, arity)
       # number of cut points = number of bins - 1
       num_cut_points = arity - 1
 
-      @features.each do |f|
-        # sort the continuous-valued attribute set
-        attributes = @columns[f].sort()
+      # sort the continuous-valued attribute set
+      attributes = attributes.sort()
 
-        min = attributes.min()
-        max = attributes.max()
+      min = attributes.min()
+      max = attributes.max()
 
-        # find the continuouse-valued range
-        range = max - min
+      # find the continuouse-valued range
+      range = max - min
 
-        # divide the range by the number of bins
-        width = range / arity
+      # divide the range by the number of bins
+      width = range / arity
 
-        #puts "Min:Max #{min}:#{max}, Range #{range} Bin Size: #{width}"
+      #puts "Min:Max #{min}:#{max}, Range #{range} Bin Size: #{width}"
 
-        # find cut points using the max of each bin
-        cut_points = []
-        (1..num_cut_points).each do |n|
+      # find cut points using the max of each bin
+      cutpoints = []
+      (1..num_cut_points).each do |n|
 
-          # the proposed cut point is the starting
-          # value of the range plus n bin widths
-          proposed_cut_point = min + (n * width)
+        # the proposed cut point is the starting
+        # value of the range plus n bin widths
+        proposed_cut_point = min + (n * width)
 
-          #puts "Min #{min}, Val #{x}, Width #{width}, Cut Point #{proposed_cut_point}"
+        cutpoints << proposed_cut_point
+      end
 
-          # the actual cut point will be the maximum
-          # value found in the real data set that is
-          # less than or equal to the proposed point
-          actual_cut_point = attributes.select do |a|
-            a <= proposed_cut_point
-          end.max()
+      cutpoints
+    end
 
-          cut_points << actual_cut_point
+    def make_cutpoints_equalwidth(arity)
+      cutpoints = {}
+      @feature_names.each do |f|
+        # get the cut points for this feature
+        feature = @feature_values[f]
+        cuts = make_feature_cutpoints_equalwidth(feature, arity)
+
+        # add this feature's cut points to the list
+        cutpoints[f] = cuts
+      end
+
+      cutpoints
+    end
+
+    def make_cutpoints_equalwidth!(arity)
+      results = make_cutpoints_equalwidth(arity)
+      @feature_cut_points = results
+    end
+
+    def make_cutpoints_equalwidth_fit!(arity)
+      make_cutpoints_equalwidth!(arity)
+      fit_bins_to_population!
+    end
+    # }}}
+
+    # Equal Length {{{
+    def make_feature_cutpoints_equallength(attributes, arity)
+      # number of cut points = number of bins - 1
+      num_cut_points = arity - 1
+
+      # sort the continuous-valued attribute set
+      attributes = attributes.sort()
+
+      # put an equal number of values in each bin
+      count = attributes.length
+      length = count.to_f / arity.to_f
+
+      # find cut points using the max of each bin
+      cutpoints = []
+      (1..num_cut_points).each do |n|
+
+        min = (n-1)*length
+        max = n*length
+
+        values = attributes.slice(min,length)
+        next_values = attributes.slice(max,length)
+
+        # the proposed cut point is the starting
+        # value of the range plus the highest
+        # value that occurs n bins from the start
+        proposed_cut_point = values.max()
+
+        # if the first value in the next bin is the
+        # same as the last value in this bin, we'll
+        # keep the cut point
+        next_proposed_point = next_values.min()
+
+        # if the next value is greater, we'll consider
+        # that to be the cut_point
+        if next_proposed_point > proposed_cut_point
+          proposed_cut_point = next_proposed_point
         end
 
-        @cut_points[f] = cut_points
+        cutpoints << proposed_cut_point
       end
+
+      cutpoints
     end
+
+    def make_cutpoints_equallength(arity)
+      cutpoints = {}
+      @feature_names.each do |f|
+        # get the cut points for this feature
+        feature = @feature_values[f]
+        cuts = make_feature_cutpoints_equallength(feature, arity)
+
+        # add this feature's cut points to the list
+        cutpoints[f] = cuts
+      end
+
+      cutpoints
+    end
+
+    def make_cutpoints_equallength!(arity)
+      results = make_cutpoints_equallength(arity)
+      @feature_cut_points = results
+    end
+
+    def make_cutpoints_equallength_fit!(arity)
+      make_cutpoints_equallength!(arity)
+      fit_bins_to_population!
+    end
+    # }}}
+    # }}}
   end
 end
--- spec/lib/rain_spec.rb
+++ spec/lib/rain_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe "rain" do
   before(:all) do
     @instances = Flower.all
-    @features  = [:sepal_len, :sepal_wid, :petal_wid, :petal_len]
+    @features  = [:sepal_len, :sepal_wid, :petal_len, :petal_wid]
 
     @discretizer = Rain::Discretizer.new(@instances, @features)
   end
 
   context "discretizer" do
     it "can create equal-width cut points" do
-      correct_cut_points = {
+      correct_equalwidth_cutpoints = {
         sepal_len: [5.2,6.1,7.0],
         sepal_wid: [2.6,3.2,3.8],
         petal_len: [1.9,3.9,5.4],
@@ -18,30 +18,40 @@ describe "rain" do
       }
 
       # this is basically an arbirary number of bins
-      arity = 4
+      num_bins = 4
 
-      @discretizer.make_cutpoints_equalwidth(arity)
-
-      @discretizer.cut_points.should == correct_cut_points
+      # we need to fit the cut points to the actual
+      # data in order to pass the test using the
+      # bin values from Iris
+      @discretizer.make_cutpoints_equalwidth_fit!(num_bins)
+      @discretizer.feature_cut_points.should == correct_equalwidth_cutpoints
     end
 
-    #it "can create equal-length cut points" do
-    #  # put an equal number of instances in each bin
-    #  correct_cut_points = {
-    #    sepal_len: [5.1,5.8,6.4],
-    #    sepal_wid: [2.8,3.0,3.3],
-    #    petal_len: [1.6,4.4,5.1],
-    #    petal_wid: [0.3,1.3,1.8]
-    #  }
+    it "can create equal-length cut points" do
+      # put an equal number of instances in each bin
+      correct_equallength_cutpoints = {
+        sepal_len: [5.1,5.8,6.4],
+        sepal_wid: [2.8,3.0,3.3],
+        petal_len: [1.6,4.4,5.1],
+        petal_wid: [0.3,1.3,1.8]
+      }
 
-    #  1.should == 1
-    #end
+      # this is basically an arbirary number of bins
+      num_bins = 4
+
+      @discretizer.make_cutpoints_equallength!(num_bins)
+      @discretizer.feature_cut_points.should == correct_equallength_cutpoints
+    end
 
     #it "can create bayesian cut points" do
     #  1.should == 1
     #end
 
     #it "can create 1-rule cut points" do
+    #  1.should == 1
+    #end
+
+    #it "can create MDLP cut points" do
     #  1.should == 1
     #end
   end
